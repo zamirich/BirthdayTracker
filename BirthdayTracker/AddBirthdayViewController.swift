f0b8bc5d8ed44f850656bce0599e91ed1d43c99a
ZamiRich <argashokovzamir@gmail.com>
2021-01-01 21:15:51 +0100

    Chapter 10 is DONE.

--- BirthdayTracker/AddBirthdayViewController.swift
+++ BirthdayTracker/AddBirthdayViewController.swift
@@ -9,14 +9,34 @@ import UIKit
 
 class AddBirthdayViewController: UIViewController {
 
-    @IBOutlet var firstNameTextField: UITextField! //this one included manually, other two will link will ctrl + drag
+    @IBOutlet var firstNameTextField: UITextField! //this one included manually, other two will link will ctrl + drag from storyboard
+    @IBOutlet weak var lastNameTextField: UITextField! //this one was connected with ctrl + drag from storyboard
+    @IBOutlet weak var birthdatePicker: UIDatePicker! //ctrl + drag as well
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        birthdatePicker.maximumDate = Date()
     }
 
+    @IBAction func saveTapped(_ sender: UIBarButtonItem) {
+        print("The save button was tapped.")
+        
+        let firstName = firstNameTextField.text ?? ""
+        let lastName = lastNameTextField.text ?? ""
+        let birthdate = birthdatePicker.date
+        
+        let newBirthday = Birthday(firstName: firstName, lastName: lastName, birthdate: birthdate)
+        
+            print("Created a Birthday!")
+            print("\(newBirthday.firstName), \(newBirthday.lastName), \(newBirthday.birthdate)")
+    }
+    
+    @IBAction func cancelTapped(_ sender: UIBarButtonItem) {
+        dismiss(animated: true, completion: nil)
+    }
+    
 
 }
 
